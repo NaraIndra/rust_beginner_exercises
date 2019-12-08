56453e43b499780193afec0ba2b73e3a9f51857a
Nara_Indra <pindak.andrey@gmail.com>
2019-12-08 04:26:47 +0300

    errors n options done

--- r1/exercises/error_handling/errorsn.rs
+++ r1/exercises/error_handling/errorsn.rs
@@ -15,30 +15,34 @@
 //
 // Execute `rustlings hint errorsn` for hints :)
 
-// I AM NOT DONE
-use std::num::ParseIntError;
+//use std::num::ParseIntError;
 use std::error;
 use std::fmt;
 use std::io;
 
 // PositiveNonzeroInteger is a struct defined below the tests.
-fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, ParseIntError> {
+fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
     let mut line = String::new();
-    b.read_line(&mut line);
-    let num: i64 = line.trim().parse()?;
-    let answer = PositiveNonzeroInteger::new(num);
-    answer
+    b.read_line(&mut line)?;
+    let num = line.trim().parse()?;
+    let answer = PositiveNonzeroInteger::new(num)?;
+    Ok(answer)
+}
+// This is a test helper function that turns a &str into a BufReader.
+fn test_with_str(s: &str) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
+    let mut b = io::BufReader::new(s.as_bytes());
+    read_and_validate(&mut b)
 }
 
 // This is a test helper function that turns a &str into a BufReader.
-fn test_with_str(s: &str) -> Result<Result<PositiveNonzeroInteger, ParseIntError>, Box<dyn error::Error>> {
-    let mut b = io::BufReader::new(s.as_bytes());
+//fn test_with_str(s: &str) -> Result<Result<PositiveNonzeroInteger, ParseIntError>, Box<dyn error::Error>> {
+//    let mut b = io::BufReader::new(s.as_bytes());
 //    b = match b {
 //    Ok(b) => b,
 //    Err(b) =>panic("wat"),
 //    };
-    read_and_validate(&mut b)
-}
+//    read_and_validate(&mut b)
+//}
 
 #[test]
 fn test_success() {
